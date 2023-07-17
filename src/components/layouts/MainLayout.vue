be12ac7324e534762a2391fb69a1ad600206bd09
loucass003 <loucass003@gmail.com>
2023-07-17 15:05:03 +0200

    Fix responsive on firefox

--- src/components/layouts/MainLayout.vue
+++ src/components/layouts/MainLayout.vue
@@ -4,17 +4,15 @@
             <div class="lg:px-6">
                 <Navbar></Navbar>
             </div>
-            <div class="w-full  border-[1px] border-app-muted bg-app-background-content aspect-video rounded-2xl relative">
-                <div
-                    class="flex h-full flex-col gap-4 justify-center xs:p-8 sm:p-12 xs:text-2xl sm:text-4xl text-app-foreground-bright animate-fade-right animate-once animate-ease-in animate-duration-500">
-                    <div>
-                        <p>Hi there! i am <span class="text-app-purple">Lucas Lelièvre</span></p>
-                        <p>i am sofware developer and a maker!</p>
-                    </div>
+            <div
+                class="flex flex-col gap-8 justify-center xs:p-8 sm:p-12 xs:text-2xl sm:text-4xl text-app-foreground-bright animate-fade-right animate-once animate-ease-in animate-duration-500 w-full border-[1px] border-app-muted bg-app-background-content aspect-video rounded-2xl relative">
+                <div>
+                    <p>Hi there! i am <span class="text-app-purple">Lucas Lelièvre</span></p>
+                    <p>i am sofware developer and a maker!</p>
+                </div>
 
-                    <div class="xs:text-xl sm:text-2xl">
-                        <ArrowLink :to="{ name: 'Home' }">About me</ArrowLink>
-                    </div>
+                <div class="xs:text-xl sm:text-2xl">
+                    <ArrowLink :to="{ name: 'Home' }">About me</ArrowLink>
                 </div>
             </div>
             <div class="lg:px-6">
